ec60a6a2b0e2939a574433abe271c665b68b485d
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-22 00:23:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,5 @@
 # Your code goes here!
 class Dog
-  def initialize(new_name, new_breed="Mutt")
-    @name = new_name
-    @breed = new_breed
-  end
   def name=(new_name)
     @name = new_name
   end
